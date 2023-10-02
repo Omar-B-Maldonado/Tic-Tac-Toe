eced112e0b3b67c86ab488ce60e1ec519eb40057
Omar Maldonado <omarm@Da-macbook.home>
2023-10-01 22:19:29 -0400

    tutorial finished, need to fix sizing and placement of Xs and Os

--- main.js
+++ main.js
@@ -2,13 +2,25 @@ console.log('Your JS is linked up. Be the person you needed when you were little
 
 /*----- Constants -----*/
 
-
+const winningCombos = 
+[
+[0, 1, 2],
+[3, 4, 5],
+[6, 7, 8],
+[0, 3, 6], 
+[1, 4, 7],
+[2, 5, 8],
+[0, 4, 8],
+[2, 4, 6]
+];
 
 /*----- App's State (Variables) -----*/
 
 let board;
 
 let turn = 'X';
+
+let win;
 
 /*----- Cached Element References -----*/
               
@@ -17,9 +29,13 @@ let turn = 'X';
 const squares = Array.from(document.querySelectorAll('#board div'));
                                                     //finding element of id: .board//
                                                     //and children: div//
+const messages = document.querySelector('h2');
+
 /*----- Event Listeners -----*/
 
 document.getElementById('board').addEventListener('click', handleTurn);
+
+document.getElementById('reset-button').addEventListener('click', init);
 
 /*----- Functions -----*/
 
@@ -46,6 +62,18 @@ function render()
             squares[index].textContent = mark;
         }        
                 );
+                if ( win === 'T' ) 
+                {
+                    messages.textContent = `That's a tie, queen!`
+                } 
+                else if (win) 
+                { 
+                    messages.textContent = `${win} wins the game!`
+                } 
+                else 
+                {
+                    messages.textContent = `It's ${turn}'s turn!`
+                }
 }
 
 function handleTurn(event) 
@@ -56,7 +84,47 @@ function handleTurn(event)
             return square === event.target;
         }
                                );
-    board[idx] = turn;        
-    console.log(board);
+    board[idx] = turn;
+
+    if (turn === 'X')
+    {
+        turn = 'O';
+    }
+    else 
+    {
+        turn = 'X';
+    }
+    win = getWinner();
+    
+    render();
+}
+
+function getWinner()
+{
+    let winner = null;
+
+    winningCombos.forEach(
+        function(combo, index)
+        {
+         if (board[combo[0]] && board[combo[0]] === 
+             board[combo[1]] && board[combo[0]] ===
+             board[combo[2]]) 
+             {
+                winner = board[combo[0]];
+             }
+        }
+                         );
+    if (winner) 
+    {
+        return winner 
+    } 
+    else if (board.includes('')) 
+    {
+        return null // if there's an empty space, return null (no winner yet)
+    } 
+    else 
+    {
+        return 'T' // no winner and no empty spaces? That's a tie!
+    }
 }
 
