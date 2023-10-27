71456604e4adf01c100b981bffbf893c07dcfc52
Omar Maldonado <omarm@Da-macbook.home>
2023-10-26 23:36:05 -0400

    Handling turns with state

--- App.js
+++ App.js
@@ -1,57 +1,93 @@
-function App()
-{
+function App() 
+{ 
     const winningCombos = 
     [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6], 
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
     ];
 
-    const [board, setBoard] = React.useState(["","","","","","","","",""])
-
-    let turn;
-
-    let win;
-
+    const [board, setBoard]       = React.useState(["","","","","","","","","",]);
+    const [turn, setTurn]         = React.useState("X");
+    const [message, setMessage]   = React.useState(`It's ${turn}'s turn!`);
+   
+    const [gameOver, setGameOver] = React.useState(false);
+    const [win, setWin]           = React.useState(null);
+  
     function handleTurn(event) 
     {
-        let i = event.target.id;
-
-        if((win == null) && ((board[i] != 'X') && (board[i] != 'O'))) // if board square is empty and there is no winner then
-        {
-            let newBoard = [...board];
-            newBoard[i] = turn;
-            setBoard(newBoard);
-
-            if (turn === 'X') turn = 'O';
-            else              turn = 'X';
-        } 
-        win = getWinner();
-        render();
+      if (gameOver) return;
+      let i        = event.target.id;
+      let newBoard = [...board];
+      newBoard[i]  = turn;
+      
+      setBoard(newBoard);
+      
+      if (newBoard[i] == 'X') setTurn('O');
+      else                    setTurn('X');
     }
-
+    
+    React.useEffect(() => {
+      let winner = getWinner();
+      setWin(winner);
+  
+      if (winner == null)  setMessage(`It's ${turn}'s turn!`);
+      else
+      {
+        setGameOver(true);
+        if (winner == "T") setMessage(`It's a tie!`);
+        else               setMessage(`${winner} wins the game!`);
+      }      
+    }, [board]);
+    
+    function handleReset() 
+    {
+      setBoard(["", "", "", "", "", "", "", "", ""]);
+      setTurn('X');
+      setGameOver(false);
+    }
+  
+    function getWinner() 
+    {
+      let winner = null;
+      winningCombos.forEach(function (combo, i) {
+        if (
+          board[combo[0]] &&
+          board[combo[0]] === board[combo[1]] &&
+          board[combo[0]] === board[combo[2]]
+        )
+          winner = board[combo[0]];
+      });
+        if (winner)             return winner;
+        if (board.includes("")) return null;
+                                return "T";
+    }
+   
     return (
-        <div>
-          <h1>Tic-React-Toe</h1>
-          <h2>It's X's turn!</h2>
-          <div class="flex-container flex-column">
-            <div class="flex-container flex-wrap" id="board" onClick={handleTurn}>
-              {board.map((value, i) => {
-                return (
-                  <div class="square" key={i} id={i}>
-                    {value}
-                  </div>
-                )
-              })}
-            </div>
-            <button id="reset-button">reset</button>
+      <div>
+        <h1>Tic-Tac-Toe</h1>
+  
+        <h2>{message}</h2>
+  
+        <div class="flex-container flex-column">
+          <div class="flex-container flex-wrap" id="board" onClick={handleTurn}>
+            {board.map((value, i) => 
+            {
+              return (
+                <div class="square" key={i} id={i}>{value}</div>
+              );
+            })}
           </div>
+  
+          <button id="reset-button" onClick={handleReset}>Reset</button>
         </div>
-      )
-}
-ReactDOM.render(<App />, root)
+      </div>
+    );
+  }
+  
+  ReactDOM.render(<App />, root);
